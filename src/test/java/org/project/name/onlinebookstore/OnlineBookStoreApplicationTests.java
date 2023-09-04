2b971a2f2e691e471ab4810d8dd9d7a1e54bb7e7
Liubomyr Kniazkyi <knyazkyylubomir@gmail.com>
2023-09-04 08:38:06 +0300

    implemented infrastructure requirements

---
+++ src/test/java/org/project/name/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.name.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
